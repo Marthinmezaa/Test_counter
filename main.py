abbae27fd4cbfae44e9de5766311f029546f8f98
Marthin Meza <juanmameza10@hotmail.com>
2025-12-02 20:10:57 -0300

    First commit: initial project structure

---
+++ main.py
@@ -0,0 +1,10 @@
+import customtkinter as ctk
+
+ctk.set_appearance_mode('dark')
+ctk.set_default_color_theme('blue')
+
+root = ctk.CTk()
+root.title('Gestor de Pruebas - Poligrafia')
+root.geometry('800x500')
+
+root.mainloop()
